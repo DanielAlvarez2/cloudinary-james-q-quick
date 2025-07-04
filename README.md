044a133eaa1d7d471a29d1163c33db34f5c48f0f
Daniel Alvarez <daniel.yllanes@hotmail.com>
2025-07-04 02:11:38 -0400

    fix bug: change app.use(cors) to app.use(cors())

--- README.md
+++ README.md
@@ -6,5 +6,6 @@ assign the port on the server a DIFFERENT port number than the front end,
 then reference the SERVER PORT NUMBER!!!  
 you will proxy the front end TO the back end  
 
+.../server$ node --watch --env-file=../.env server.js
 
 
--- server/server.js
+++ server/server.js
@@ -1,17 +1,25 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-app.use(cors)
-app.use(express.json({limit:'50mb'}))
+const {cloudinary} = require('./utils/cloudinary.js')
+app.use(cors())
+app.use(express.json({limit:'50mb'})) // allows parsing of 'body' data, i.e. req.body.data
 app.use(express.urlencoded({limit:'50mb',extended:true}))
-app.post('/api/upload',(req,res)=>{
+console.log('test')
+console.log(process.env.CLOUDINARY_NAME)
+app.post('/api/upload',async(req,res)=>{
     try{
         const fileStr = req.body.data
-        console.log(fileStr)
+        const uploadedResponse = await cloudinary.uploader.upload(fileStr, {
+            upload_preset:'dev_setups',
+        })
+        console.log(uploadedResponse)
+        res.json({msg:'YAYAYAYAY'})
     }catch(err){
         console.log(err)
+        res.status(500).json({err:'Something went wrong'})
     }
 })
 app.listen(5175,()=>{
-    console.log('Express Server Running: PORT 5174')
+    console.log('Express Server Running: PORT 5175')
 })
